a5bfb4d41d837722c46d8cc42d324714e61d7d72
vakobzar <vk283@nyu.edu>
2016-09-08 14:12:07 -0400

    Create convex_example.jl

---
+++ convex_example.jl
@@ -0,0 +1,22 @@
+#from http://convexjl.readthedocs.io/en/latest/
+using Convex
+
+# Generate random problem data
+m = 4;  n = 5
+A = randn(m, n); b = randn(m, 1)
+
+# Create a (column vector) variable of size n x 1.
+x = Variable(n)
+
+# The problem is to minimize ||Ax - b||^2 subject to x >= 0
+# This can be done by: minimize(objective, constraints)
+problem = minimize(sumsquares(A * x - b), [x >= 0])
+
+# Solve the problem by calling solve!
+solve!(problem)
+
+# Check the status of the problem
+problem.status # :Optimal, :Infeasible, :Unbounded etc.
+
+# Get the optimum value
+problem.optval
